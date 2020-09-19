3d9e9c41525bbd1761e2e04c1d871035dba6f024
Gustavo Santos <gugahh.br@gmail.com>
2020-09-19 03:19:19 -0300

    Separando App em um arquivo à parte

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+class App extends React.Component {
+
+    // Constructor eh o primeiro metodo a ser executado na isntancia (precede Render.)
+    constructor(props) {
+
+        super(props); // Obrigatório no Construtor!
+
+        /* Aqui no construtor é o UNICO momento em que é
+           permitido atribuir diretamente o state! */
+        this.state = {
+            latitude: null,
+            errorMessage: null
+        };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ latitude: position.coords.latitude });
+            },
+            (err) => {
+                this.setState({ errorMessage: err.message });
+            }
+        );
+    }
+
+    // Obs: Implementar o metodo RENDER eh obrigatorio!
+    render() {
+        if (this.state.latitude && !this.state.errorMessage) {
+
+            return <div>Latitude: {this.state.latitude}</div>;
+
+        } else if (!this.state.latitude && this.state.errorMessage) {
+
+            return <div>ErrorMessage: {this.state.errorMessage}</div>;
+            
+        }
+        return <div>Loading...</div>;
+    }
+
+}
+
+export default App;
