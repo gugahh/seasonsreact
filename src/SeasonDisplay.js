c42862b5863d6b65c89dc24b5889fae78869df9a
Gustavo Santos <gugahh.br@gmail.com>
2020-09-19 16:27:42 -0300

    Próximo ao final do exercício

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -11,38 +11,22 @@ const SeasonDisplay = (props) => {
 
     const mesAtual = new Date().getMonth() + 1;
 
-    const mesQuente = 
+    var mesQuente = 
     (
-        true
-        //(hemisferio === 'Norte')
-
-    );
-
-    /*
-            || (hemisferio === 'Sul')
-    Norte  && mesAtual >= 4 && mesAtual <= 9
-    Sul && (mesAtual >= 10 || mesAtual <= 3)
-    */
-
-    console.log('Hemisfério: ' + hemisferio);
-    console.log('Mes quente: ' + mesQuente);
-    console.log('Norte '  + (hemisferio === 'Norte' && mesAtual >= 4 && mesAtual <= 8));
-    console.log('Sul '  + (hemisferio === 'Sul' && (mesAtual >= 10 || mesAtual <= 3)));
+        (hemisferio === 'Sul' && ( mesAtual >= 10 || mesAtual <= 3)) 
+        || (hemisferio === 'Norte' && mesAtual >= 4 && mesAtual <= 9)
+    ) ? true : false;
 
     var climaLocal = ( (mesQuente) 
         ? 'Está quente aí, não?' 
         : 'Opa, deve estar bem frio, hein?');
-
-    console.log('Mes quente: ' + mesQuente);
-
-    console.log('Clima Local: ' + climaLocal);
 
     return (
         <div>
             <p>Hemisfério: <b>{hemisferio}</b></p>
             <p>Mês: <b>{mesAtual}</b></p>
             <p>Mês Quente: {mesQuente ? 'true' : 'false'}</p>
-            <p>{climaLocal}</p>
+            <h3>{climaLocal}</h3>
         </div>
      ) ;
 }
