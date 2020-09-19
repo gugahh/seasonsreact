ccd6e77c4f6a65b045c8b438211d9eebac014472
Gustavo Santos <gugahh.br@gmail.com>
2020-09-19 17:01:22 -0300

    adicionando icones

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,32 +1,33 @@
 import React from 'react';
 
-const SeasonDisplay = (props) => {
-    var hemisferio = null;
+const HEMISF_SUL = 'Sul';
+const HEMISF_NORTE = 'Norte';
 
-    if (props.latitude <= 0) {
-        hemisferio = 'Sul';
-    } else {
-        hemisferio = 'Norte';
-    }
+const SeasonDisplay = (props) => {
+
+    const hemisferio = (props.latitude <= 0) 
+        ? HEMISF_SUL
+        : HEMISF_NORTE;
 
     const mesAtual = new Date().getMonth() + 1;
 
-    var mesQuente = 
-    (
-        (hemisferio === 'Sul' && ( mesAtual >= 10 || mesAtual <= 3)) 
-        || (hemisferio === 'Norte' && mesAtual >= 4 && mesAtual <= 9)
+    const mesQuente = (
+        (hemisferio === HEMISF_SUL && ( mesAtual >= 10 || mesAtual <= 3)) 
+        || (hemisferio === HEMISF_NORTE && mesAtual >= 4 && mesAtual <= 9)
     ) ? true : false;
 
-    var climaLocal = ( (mesQuente) 
+    const climaLocal = mesQuente 
         ? 'Está quente aí, não?' 
-        : 'Opa, deve estar bem frio, hein?');
+        : 'Opa, deve estar bem frio, hein?';
 
+
+    const iconStyle = mesQuente ? `sun icon` : `snowflake icon`;
+        
     return (
         <div>
-            <p>Hemisfério: <b>{hemisferio}</b></p>
-            <p>Mês: <b>{mesAtual}</b></p>
-            <p>Mês Quente: {mesQuente ? 'true' : 'false'}</p>
-            <h3>{climaLocal}</h3>
+            <i className={iconStyle} />
+            <h1>{climaLocal}</h1>
+            <i className={iconStyle} />
         </div>
      ) ;
 }
