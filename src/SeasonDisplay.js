4bf4a7fcbc4e6c8c3d3afb0983707190c7463fe0
Gustavo Santos <gugahh.br@gmail.com>
2020-09-20 13:25:11 -0300

    Estilo CSS e simplificação do código

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,35 +1,49 @@
+import './css/SeasonDisplay.css';
 import React from 'react';
 
-const HEMISF_SUL = 'Sul';
-const HEMISF_NORTE = 'Norte';
+const seasonConfig = {
+    summer: {
+        text: 'Let\'s hit the beach!',
+        iconName: 'sun outline'
+    },
+    winter: {
+        text: 'Damn, it\'s cold in here!',
+        iconName: 'snowflake outline'
+    }
+};
 
 const SeasonDisplay = (props) => {
 
-    const hemisferio = (props.latitude <= 0) 
-        ? HEMISF_SUL
-        : HEMISF_NORTE;
-
+    const latitude = props.latitude;
     const mesAtual = new Date().getMonth() + 1;
 
-    const mesQuente = (
-        (hemisferio === HEMISF_SUL && ( mesAtual >= 10 || mesAtual <= 3)) 
-        || (hemisferio === HEMISF_NORTE && mesAtual >= 4 && mesAtual <= 9)
-    ) ? true : false;
-
-    const climaLocal = mesQuente 
-        ? 'Está quente aí, não?' 
-        : 'Opa, deve estar bem frio, hein?';
-
-
-    const iconStyle = mesQuente ? `sun icon` : `snowflake icon`;
+    const season = getSeason(latitude, mesAtual);
+    const { text, iconName, iconColor } = seasonConfig[season];
         
     return (
-        <div>
-            <i className={iconStyle} />
-            <h1>{climaLocal}</h1>
-            <i className={iconStyle} />
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left ${iconName} icon massive`} />
+            <h1>{text}</h1>
+            <i className={`icon-right ${iconName} icon massive`} />
         </div>
      ) ;
+
+     /**
+      * Obtem a 'estacao do ano', baseado em latitude e o mes corrente.
+      * Retorna 'summer' (verao) ou 'winter' (inverno).
+      * @param {*} latitude 
+      * @param {*} mesAtual 
+      */
+     function getSeason(latitude, mesAtual) {
+        var estacao = (
+            //Hemisferio Sul:
+            ( (latitude <= 0) && ( mesAtual >= 10 || mesAtual <= 3))  
+            //Hemisferio norte
+            || ((latitude > 0) && mesAtual >= 4 && mesAtual <= 9) 
+        ) ? 'summer' : 'winter';
+
+        return estacao;
+     }
 }
 
 export default SeasonDisplay;
